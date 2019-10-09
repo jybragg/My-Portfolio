cb497815e06c3f373bece865cf0bf0be8c3bb022
Jasmin <jasymatos@gmail.com>
2019-10-09 11:42:02 -0400

    edited layout on main pg and added animation to buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,44 @@
 console.log("I am connected");
 
-// var fullpage = require("fullpage.js");
+// (function($) {
+//     "use strict"; // Start of use strict
 
-// module.exports = fullpage;
+// Smooth scrolling using jQuery easing
+// $(".js-scroll-trigger").click(function () {
+//     console.log("btn working");
+//     //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//     var target = $(this.hash);
+//      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+//     $('html, body').animate({
+//         scrollTop: (target.offset().top - 72)
+//     }, 1000, "easeInOutExpo");
+
+// });
+
+//   })(jQuery); // End of use strict
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".js-scroll-trigger").on('click', function(event) {
+        console.log("btn working");
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
