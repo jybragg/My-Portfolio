65a65b187acfeb4e5e290587abf54210be5cc288
Jasmin <jasymatos@gmail.com>
2019-11-11 12:16:23 -0500

    minor styling fixes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,5 @@
 console.log("I am connected");
 
-// (function($) {
-//     "use strict"; // Start of use strict
-
-// Smooth scrolling using jQuery easing
-// $(".js-scroll-trigger").click(function () {
-//     console.log("btn working");
-//     //   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//     var target = $(this.hash);
-//      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-//     $('html, body').animate({
-//         scrollTop: (target.offset().top - 72)
-//     }, 1000, "easeInOutExpo");
-
-// });
-
-//   })(jQuery); // End of use strict
-
 $(document).ready(function(){
     // Add smooth scrolling to all links
     $(".js-scroll-trigger").on('click', function(event) {
@@ -42,3 +24,5 @@ $(document).ready(function(){
       } // End if
     });
   });
+
+  
